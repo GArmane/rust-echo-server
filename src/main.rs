8ecd1efa873fcbc1f32867c685f0afd40293149b
Giovanni Armane <giovanniarmane@gmail.com>
2024-10-30 01:59:54 -0300

    feat(http-server): base TCP connection listening

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+use std::io::Result;
+
+fn main() -> Result<()> {
+    let addr = "127.0.0.1:7878";
+    let listener = TcpListener::bind(addr).unwrap();
+
+    for stream in listener.incoming() {
+        stream.unwrap();
+        println!("Connection established");
+    }
+
+    Ok(())
 }
+
